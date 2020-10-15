1ad46bc3cb427eaea7f6401bff91847fc6e7f258
Glitch (gaudy-topaz-navy) <none>
2020-10-15 20:26:45 +0000

    🐄🍱 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,17 @@
+exports.TOKEN = 'NzQ0NTk3Mzc3NDA2NTk5MTg4.XzliWA.eKZukq4NYtoi2q2dpqBM8s690sw';
+
+exports.PREFIX = 'q';
+
+exports.GOOGLE_API_KEY = 'AIzaSyCpgbji1klCW_v6220mRyr3eY-WIFmXOKE';
+
+exports.GENIUS_API_KEY = 'zdTLphFCy4pAlmK1mWEIn5Sw7B1N38XUp8UDeJcUGMavINCWSF3W9VakeQI8Z202';
+
+exports.yandex_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+
+exports.news_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+
+exports.giphy_API = 'CaJxgWS4z2CImmnPsbfLBuGTWLkmElVx';
+
+exports.AME_API = '8428879cb2b45d90d501ca689d3a6a6d46261721e6d0127fa8e83ae3954fb531d29b9ae09886e30833364330494e6234174b973feab81e8b269c0377c670657c';
+
+exports.blague_API = 'rwfcDdWYMrMfdy9WvihcQeEO1-R_s1eh7hNttZFIzPEq5EZDcH2g3WiaO0ccyVml';
